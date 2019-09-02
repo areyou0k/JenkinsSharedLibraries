dc7ad3ec846027963c4172e4d7933e820dc6538b
BruceLEO1969 <bruceleo1969@gmail.com>
2019-09-02 16:51:56 +0800

    git log pretty

--- vars/pipelineIosAppDevelopBranch.groovy
+++ vars/pipelineIosAppDevelopBranch.groovy
@@ -109,7 +109,7 @@ def call(Closure body={}) {
 def buildDeveopBranch() {
     echo "env previous: ${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}"
     echo "env GIT_COMMIT: ${env.GIT_COMMIT}"
-    sh 'git log ${env.GIT_PREVIOUS_SUCCESSFUL_COMMIT}..${env.GIT_COMMIT}'
+    sh 'git log --pretty="> %s%n" acdfe33f4...3002d0938 --no-merges'
     echo "Develop branch - Build"
     sh 'bundle install'
     sh 'bundle exec fastlane ios do_publish_all'
