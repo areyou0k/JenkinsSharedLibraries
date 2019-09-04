3f23bb8efd65953fa492b8bb5d0b65489bd8d9cc
BruceLEO1969 <bruceleo1969@gmail.com>
2019-09-04 16:04:47 +0800

    timeout 1 h

--- vars/pipelineAndroidAppTestBranch.groovy
+++ vars/pipelineAndroidAppTestBranch.groovy
@@ -11,6 +11,7 @@ def call(Closure body={}) {
         agent none
 
         options {
+            timeout(time: 1, unit: 'HOURS')
             skipDefaultCheckout()
             ansiColor('xterm')
             retry(3)
